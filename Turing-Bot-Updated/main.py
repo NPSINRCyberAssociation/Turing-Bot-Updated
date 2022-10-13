0db103d655eee9342ff2cf7918dcc2bc5a9d53f6
Harikeshav R <r.harikeshav@gmail.com>
2022-10-13 17:25:30 +0530

    Create basic bot

---
+++ Turing-Bot-Updated/main.py
@@ -0,0 +1,32 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from rich.logging import RichHandler
+
+from bot.bot import Bot
+
+load_dotenv()
+
+# Get discord token
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Setup logging using rich's logging handler.
+FORMAT = "%(message)s"
+
+logging.basicConfig(
+    level=logging.INFO, format=FORMAT, datefmt="[%X]", handlers=[RichHandler(markup=True)]
+)
+
+logger = logging.getLogger()
+
+
+def main(token):
+    bot = Bot()
+
+    # Run the bot.
+    bot.run(token)
+
+
+if __name__ == "__main__":
+    main(DISCORD_TOKEN)
